56db346343878791f28b5a6d4d5535a8cd77c1de
Matt Carlson <matthewthomascarlson@gmail.com>
2015-10-17 03:39:56 +0000

    update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,17 @@
 $(document).ready(function() {
-  $(".btn").click(function(event) {
-    var cityInput = $("#city").val();
-    var musicInput = $("#music").val();
-    var tvInput = $("#tv").val();
-    var philosopherInput = $("#philosopher").val();
-    
-    $(".city").text(cityInput);
-    $(".music").text(musicInput);
-    $(".tv").text(tvInput);
-    $(".philosopher").text(philosopherInput);
-    
-    $("#answers").show();
-    event.preventDefault();
-  });
+  var highNumber = parseInt(prompt("Give me a number to ping pong to."));
+  for (var x=1; x<= highNumber; x++) {
+    if ( x % 3 && x % 5 ) {
+      document.write(x);
+    } else {
+      if (x % 3 == 0) {
+        document.write("ping");
+      }
+      if ( x % 5 == 0) {
+        document.write("pong");
+      }
+    }
+    document.write('<br>');
+  }
 });
+
