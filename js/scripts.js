949505ce325382173b0ce388a1628a25340b3f04
Matt Carlson <matthewthomascarlson@gmail.com>
2015-10-22 22:27:04 +0000

    change scripts.js to insert html into DOM instead of with document.write

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,20 @@ $(document).ready(function() {
   var highNumber = parseInt(prompt("Give me a number to ping pong to."));
   for (var x=1; x<= highNumber; x++) {
     if ( x % 3 && x % 5 ) {
-      document.write(x);
+       $("p").append(x);
     } else {
       if (x % 3 == 0) {
-        document.write("ping");
+        $("p").append("ping");
       }
       if ( x % 5 == 0) {
-        document.write("pong");
+        $("p").append("pong");
       }
     }
-    document.write('<br>');
+    $("p").append("<br>");
   }
 });
+
+
+
+
 
